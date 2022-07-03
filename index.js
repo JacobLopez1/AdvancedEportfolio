63de4f1307b37c37999e5c4ecbbfd1eaff208c3e
Jacob Lopez <lopezjacob2003.jl@gmail.com>
2022-07-02 18:27:02 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_0o61afv",
+      "template_8x9pzld",
+      event.target,
+      "krBQzk9i33e5qqdlJ"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly: lopezjacob2003.jl@gmail.com"
+      );
+    });
+}
